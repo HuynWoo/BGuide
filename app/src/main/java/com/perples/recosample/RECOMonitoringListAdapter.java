c3cd17ee595fbcd94f09aab76b09d6e73f402e5e
HuynWoo <joi0608@nate.com>
2015-06-14 05:36:24 +0900

    edit layout design

--- app/src/main/java/com/perples/recosample/RECOMonitoringListAdapter.java
+++ app/src/main/java/com/perples/recosample/RECOMonitoringListAdapter.java
@@ -48,8 +48,6 @@ public class RECOMonitoringListAdapter extends BaseAdapter {
     String recoUpdateTime;
     String recoBeaconCount;
 
-    //private RECORangingListAdapter mRangingListAdapter;
-    private ArrayList<RECOBeacon> mRangedBeacons;
     private LayoutInflater mLayoutInflater;
 	
 	public RECOMonitoringListAdapter(Context context) {
@@ -110,8 +108,6 @@ public class RECOMonitoringListAdapter extends BaseAdapter {
 		RECOBeaconRegion recoRegion = mMonitoredRegionLists.get(position);
 		RECOBeaconRegionState recoState = mMonitoredRegions.get(recoRegion);
 
-        RECOBeacon recoBeacon = mRangedBeacons.get(position);
-		
 		recoRegionUniqueID = recoRegion.getUniqueIdentifier();
 		recoRegionState = recoState.toString();
 		recoUpdateTime = mLastUpdateTime.get(recoRegion);
@@ -121,7 +117,6 @@ public class RECOMonitoringListAdapter extends BaseAdapter {
 		viewHolder.recoRegionState.setText(recoRegionState);
 		viewHolder.recoRegionTime.setText(recoUpdateTime);
 		viewHolder.recoRegionBeaconCount.setText("# of beacons in the region: " + recoBeaconCount);
-        viewHolder.recoAccuracy.setText(String.format("%.2f", recoBeacon.getAccuracy()));
 
 		return convertView;
 	}
@@ -131,6 +126,5 @@ public class RECOMonitoringListAdapter extends BaseAdapter {
 		TextView recoRegionState;
 		TextView recoRegionTime;
 		TextView recoRegionBeaconCount;
-        TextView recoAccuracy;
 	}
 }

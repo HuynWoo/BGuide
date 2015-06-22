ca4759f6b5a7e893eadb3d34e1012e8712f90755
soyokim <soyeon120129@gmail.com>
2015-06-23 00:48:39 +0900

    12

---
+++ app/src/main/java/com/perples/recosample/ContentActivity.java
@@ -0,0 +1,17 @@
+package com.perples.recosample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by uk2014 on 2015-06-23.
+ */
+public class ContentActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_view);
+
+
+    }
+}
